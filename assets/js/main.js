40fe27c96b2b1d004ed53adcf6232886cf8456b0
Jean Lucien RANDRIANANTENAINA <rjlucienaina@gmail.com>
2022-05-03 21:21:30 +0300

    🚀 Amimation de base dans index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
     $(function() {
-        $(".titre").animate({ left: '10%' }, 500);
+        $(".titre").animate({ left: '0%' }, 1500);
+        $("#hero > .img-title").animate({ 'background-position-x': '80%' }, 1500);
+        $(".description").animate({ "opacity": "1" }, 2000);
+
     })
+    $(".description").click(function() { $(this).fadeIn(); });
+
+    $(window).scroll(function() {
+        let stop = $("#spiderman").offset().top;
+        let positionY = Math.floor($(document).scrollTop());
+        if (positionY < stop) {
+            $(".img-title2").animate({ 'top': positionY + "px" });
+        }
+    });
 
     const finDecompte = new Date("May 6, 2022 08:00:00").getTime()
     let interval = setInterval(function() {
@@ -20,17 +32,18 @@ $(document).ready(function() {
         }
     }, 1000);
 
-    $(".button1").mouseenter(function() {
+
+
+    $(".button1").hover(function() {
         $(this).css({
             'background-color': 'rgba(0,0,0,0)',
             'color': 'var(--white-ad-color)',
-            'box-shadow': '0px 0px 10px var(--white-ad-color)',
+            'box-shadow': '0px 0px 1rem var(--white-ad-color)',
             'border': '2 px solid var(--white-ad-color)',
             'font-weight': 'bold'
         })
-    });
 
-    $(".button1").mouseleave(function() {
+    }, function() {
         $(this).css({
             'background-color': 'var(--white-ad-color)',
             'color': 'var(--primary-ad-color)',
@@ -40,33 +53,67 @@ $(document).ready(function() {
         })
     });
 
-    $(".cta .button2,input.btn").mouseenter(function() {
-        $(this).addClass('button2-invert')
-    });
-    $(".cta .button2,input.btn").mouseleave(function() {
-        $(this).removeClass('button2-invert')
-    });
-    $("footer .media img").mouseenter(function() {
-        $(this).css({ 'filter': 'brightness(100%)' })
-    });
-    $("footer .media img").mouseleave(function() {
-        $(this).css({ 'filter': 'invert(0)' })
+    $(".button2").hover(function() {
+        $(this).css({
+            'background-color': 'var(--white-ad-color)',
+            'color': 'var(--primary-ad-color)',
+            'box-shadow': '0px 0px 1rem var(--white-ad-color)',
+            'border': 'initial',
+            'font-weight': 'initial'
+        })
+    }, function() {
+        $(this).css({
+            'background-color': 'initial',
+            'color': 'var(--white-ad-color)',
+            'box-shadow': 'initial',
+            'border': 'var(--white-ad-color) 2px solid',
+            'font-weight': 'initial'
+        })
     });
 
+
+    $(".button3").hover(function() {
+        $(this).css({
+            'background-color': 'var(--white-ad-color)',
+            'color': 'var(--blue-color)',
+            'box-shadow': '0px 0px 1rem var(--white-ad-color)',
+            'border': 'initial',
+            'font-weight': 'initial'
+        })
+    }, function() {
+        $(this).css({
+            'background-color': 'initial',
+            'color': 'var(--white-ad-color)',
+            'box-shadow': 'initial',
+            'border': 'var(--white-ad-color) 2px solid',
+            'font-weight': 'initial'
+        })
+    });
+
+    $(".nav-link").hover(function() {
+        $(this).css('text-decoration', 'line-through');
+
+    }, function() {
+        $(this).css('text-decoration', 'initial');
+    });
+
+
     /* SpiderMan Cimena */
-    $("#spider1").mouseenter(function() {
+    $("#spider1").mouseover(function() {
         $(this, ">img").css({ "transform": "scale(1.05)" });
         $("#spider1 > .card-body").slideDown(1500)
     });
-    $("#spider2").mouseenter(function() {
+    $("#spider2").mouseover(function() {
         $(this, ">img").css({ "transform": "scale(1.05)" });
-        $("#spider2 > .card-body").slideDown(1500)
+        $("#spider2 > .card-body").slideDown(1500);
     });
-    $("#spider3").mouseenter(function() {
+    $("#spider3").mouseover(function() {
         $(this, ">img").css({ "transform": "scale(1.05)" });
         $("#spider3 > .card-body").slideDown(1500)
     });
 
+
+    /*
     $("#spider1").mouseleave(function() {
         $(this, ">img").css({ "transform": "scale(1)" });
         $("#spider1 > .card-body").slideUp(1500)
@@ -78,6 +125,16 @@ $(document).ready(function() {
     $("#spider3").mouseleave(function() {
         $(this, ">img").css({ "transform": "scale(1)" });
         $("#spider3 > .card-body").slideUp(1500)
-    });
+    });*/
 
+    $('[type="submit"]').mouseenter(function() {
+
+    })
+
+    $('[type="submit"]').click(function() {
+        $("#overlay").show();
+    });
+    $("#form-pop-up").click(function() {
+        $("#overlay").hide();
+    });
 });
