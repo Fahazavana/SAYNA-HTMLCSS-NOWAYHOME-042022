33bedbb1e805282f6a349d13c07ac07e2a7f3192
Jean Lucien RANDRIANANTENAINA <rjlucienaina@gmail.com>
2022-05-06 08:39:10 +0300

    👌 refactoring hero.html

--- assets/js/hero.js
+++ assets/js/hero.js
@@ -1,27 +1,26 @@
 $(document).ready(function() {
-    var $i = 0
+    let $sliderItem = $(".slider-item");
+    let $now = 0;
+
     $(function() {
-        function changeNow() {
-            if ($i == 5) {
-                $i = 1;
-                $slideNow = "slide5";
-                $slide = "slide" + $i;
-            } else {
-                $slideNow = "slide" + ($i);
-                $i++;
-                $slide = "slide" + $i;
+        function slide() {
+            for (var i = 0; i < $sliderItem.length; i++) {
+                console.log($($sliderItem[i]))
+                $($sliderItem[i]).css('display', 'none')
             }
-            $("#avengers").removeClass($slideNow).addClass($slide)
+            $now++;
+            ($now > $sliderItem.length) ? ($now = 1) : {};
+            $($sliderItem[$now - 1]).css('display', 'block')
         }
-        setInterval(changeNow, 5000)
-    });
+        setInterval(slide, 5000)
+    })
 
 
-
-    $("img").hover(function() {
+    $("img:not(.ic-1,.slider-item)").hover(function() {
         $(this).toggleClass("zoom")
 
     }, function() {
         $(this).toggleClass("zoom")
     });
+
 });
